96f6e07a465de11442984c69ecf8dc2a51dfc249
teyvi <teyviangela@yahoo.com>
2026-01-05 16:10:59 +0000

    add nodemailer dependency and update main entry point to TypeScript in package.json

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/routes";
+import nodemailer from "nodemailer";
 
 const app = express();
+
 //global middlewares
 app.use(cors());
 app.use(express.json());
